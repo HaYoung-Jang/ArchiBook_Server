67dcc02319a15002702285f44bf78dc1eecafa86
HaYoung-Jang <wkd7198@naver.com>
2021-11-04 15:25:58 +0900

    Project : Update

--- main/src/main/java/com/archibook/main/mapper/ChatMapper.java
+++ main/src/main/java/com/archibook/main/mapper/ChatMapper.java
@@ -12,5 +12,5 @@ public interface ChatMapper {
 	public int insertChat(ChatDTO params);
 	public ChatDTO selectChatDetail(int chatId);
 	public List<ChatDTO> selectChatList(String accountId);
-	public List<ChatDTO> selectChatSold(int registerdId, String accountId);
+	public List<ChatDTO> selectChatSold(int registerd_id, String account_id);
 }
